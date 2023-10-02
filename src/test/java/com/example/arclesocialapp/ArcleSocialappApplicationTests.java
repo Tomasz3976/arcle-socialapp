e9a051433cfe5b66429e33dbbd10919e5b75a65b
Tomasz3976 <tomeczek2886@gmail.com>
2023-10-02 18:30:41 +0200

    initial commit

---
+++ src/test/java/com/example/arclesocialapp/ArcleSocialappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.arclesocialapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArcleSocialappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
